26d39d01736cdec8c527ac091a6ccba02076d509
Aj163 <a3sajs@gmail.com>
2018-03-16 16:07:58 +0530

    Update student and teacher

--- src/teacher.cpp
+++ src/teacher.cpp
@@ -6,9 +6,10 @@ using namespace std;
 void uploadQuestion()
 {
 	system("clear");
-	string readLine, questionPath, inputPath, outputPath;
+	string readLine, questionPath, testCasesFolderPath, inputPath, outputPath;
 	string question = "", testInput = "", testOutput = "";
 	ifstream questionFile, inputFile, outputFile;
+	int numberOfTestCases;
 
 	//Question file
 	printf("Enter path to question file : ");
@@ -24,33 +25,53 @@ void uploadQuestion()
 
 	questionFile.close();
 
-	//Input test cases file
-	printf("Enter path to input test cases file : ");
-	cin>>inputPath;
-	inputFile.open(inputPath);
 
-	while(!inputFile.eof())
+	printf("Enter number of test cases : ");
+	cin>>numberOfTestCases;
+
+	printf("Enter path to folder containing all test cases : ");
+	cin>>testCasesFolderPath;
+
+	testInput = to_string(numberOfTestCases);
+	testInput += '\n';
+	for(int i=0; i<numberOfTestCases; i++)
 	{
-		getline(inputFile, readLine);
-		question += readLine;
-		question += "\n";
+		//Input test cases file
+		inputPath = testCasesFolderPath + "/input";
+		inputPath += '0'+i/10;
+		inputPath += '0'+i%10;
+		inputPath += ".txt";
+
+		inputFile.open(inputPath);
+		while(!inputFile.eof())
+		{
+			getline(inputFile, readLine);
+			testInput += readLine;
+			testInput += "\n";
+		}
+
+		testInput += "#\n";
+		inputFile.close();
+
+		//Input test cases file
+		outputPath = testCasesFolderPath + "/output";
+		outputPath += '0'+i/10;
+		outputPath += '0'+i%10;
+		outputPath += ".txt";
+
+		outputFile.open(outputPath);
+		while(!outputFile.eof())
+		{
+			getline(outputFile, readLine);
+			testOutput += readLine;
+			testOutput += "\n";
+		}
+
+		testOutput += "#\n";
+		outputFile.close();
 	}
 
-	questionFile.close();
-
-	//Output test case file
-	printf("Enter path to question file : ");
-	cin>>questionPath;
-	questionFile.open(questionPath);
-
-	while(!questionFile.eof())
-	{
-		getline(questionFile, readLine);
-		question += readLine;
-		question += "\n";
-	}
-
-	questionFile.close();
+	//Upload question, testInput, testOutput
 }
 
 void getReport(){}
