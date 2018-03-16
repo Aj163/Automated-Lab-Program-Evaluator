29a01b14561b6c585b582a891bbbddd2ee8f6124
Aj163 <a3sajs@gmail.com>
2018-03-16 15:36:10 +0530

    update teacher

--- src/teacher.cpp
+++ src/teacher.cpp
@@ -3,11 +3,60 @@
 #include <unistd.h>
 using namespace std;
 
-void uploadQuestion(){}
+void uploadQuestion()
+{
+	system("clear");
+	string readLine, questionPath, inputPath, outputPath;
+	string question = "", testInput = "", testOutput = "";
+	ifstream questionFile, inputFile, outputFile;
+
+	//Question file
+	printf("Enter path to question file : ");
+	cin>>questionPath;
+	questionFile.open(questionPath);
+
+	while(!questionFile.eof())
+	{
+		getline(questionFile, readLine);
+		question += readLine;
+		question += "\n";
+	}
+
+	questionFile.close();
+
+	//Input test cases file
+	printf("Enter path to input test cases file : ");
+	cin>>inputPath;
+	inputFile.open(inputPath);
+
+	while(!inputFile.eof())
+	{
+		getline(inputFile, readLine);
+		question += readLine;
+		question += "\n";
+	}
+
+	questionFile.close();
+
+	//Output test case file
+	printf("Enter path to question file : ");
+	cin>>questionPath;
+	questionFile.open(questionPath);
+
+	while(!questionFile.eof())
+	{
+		getline(questionFile, readLine);
+		question += readLine;
+		question += "\n";
+	}
+
+	questionFile.close();
+}
+
 void getReport(){}
 void viewSubmissionOfStudent(){} //Get file also
 
 int main()
 {
-
+	uploadQuestion();
 }
